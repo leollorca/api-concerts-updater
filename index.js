297c13352bfdfd8a2c75384ce757496d3f8b00f6
Léo Llorca <leollorca@macbook-air-de-leo.home>
2022-10-27 22:39:33 +0200

    fix port issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./server");
 
 function start() {
   server
-    .listen({ port: process.env.PORT || 8070 })
+    .listen(process.env.PORT, "0.0.0.0")
     .then(() => {
       console.info("Server is running");
     })
