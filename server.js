1c9c6acc4b00106d1a63cd3c34a2e6371adbb69e
Léo Llorca <leollorca@macbook-air-de-leo.home>
2022-10-29 18:49:43 +0200

    try to fix origins

--- server.js
+++ server.js
@@ -16,9 +16,10 @@ const server = fastify({
   },
 });
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",") || "";
-
-server.register(cors, { credentials: true, origin: allowedOrigins });
+server.register(cors, {
+  credentials: true,
+  origin: process.env.CLIENT_URL || process.env.SITE_URL,
+});
 
 server.register(cookie, {
   secret: process.env.COOKIE_SECRET,
